89d13ef085e1f4a736fc42eac390f1d7943386b7
Mohsin Kaleem <mohkale@kisara.moe>
2020-08-01 23:02:06 +0100

    (client): switch to momentjs for time formatting
    It's seriously insane how there's no built in datetime format that
    isn't a pain to use. momentjs exists so it is possible. That said,
    date time is a serious pain so its understandable no browser or
    standard is willing to incorporate some interface which it'll probably
    deprecate 2 years down the line (ahhmmm, ahhhmmmm java).

--- src/utils/methods/time-format.ts
+++ src/utils/methods/time-format.ts
@@ -1,4 +1,32 @@
+import moment from 'moment';
+
+const formatInt = (int: number): string => {
+  if (int < 10) {
+    return `0${int}`;
+  }
+
+  return `${int}`;
+};
+
+// source: [[https://github.com/moment/moment/issues/463#issuecomment-552498641][#463]].
+export const formatDuration = (time: number): string => {
+  const duration = moment.duration(time)
+  const seconds = duration.seconds(),
+        minutes = duration.minutes(),
+        hours = duration.hours();
+
+  if (hours > 0) {
+    return `${formatInt(hours)}:${formatInt(minutes)}:${formatInt(seconds)}`;
+  }
+
+  if (minutes > 0) {
+    return `${formatInt(minutes)}:${formatInt(seconds)}`;
+  }
+
+  return `00:${formatInt(seconds)}`;
+};
+
 // https://github.com/tremc/tremc/blob/master/tremc#L3362
 export function timeFormat(duration: number): string {
-  return duration.toString()
+  return formatDuration(duration * 1000)
 }
